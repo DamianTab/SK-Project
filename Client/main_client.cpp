ba93e58f9988eb781caf0c727e5d28e7a13f8d9b
Damian Tabaczynski <dam.tab@wp.pl>
2020-01-05 16:30:18 +0100

    refactor code, add map for sockets, add errors messages

--- Client/main_client.cpp
+++ Client/main_client.cpp
@@ -5,27 +5,31 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <errno.h>
- 
+#include <error.h>
+
 #define BUFFER_SIZE 512
  
 using namespace std;
- 
-int main()
-{
-    sockaddr_in adres;
+
+int main(int argc, char **argv) {
     char duzybufor[BUFFER_SIZE];
-   
-    inet_aton("127.0.0.1", &adres.sin_addr);
-        adres.sin_port = htons(3000);
-        adres.sin_family = PF_INET;
 
-    int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (argc != 3)
+        error(1, 0, "To correctly run type: %s <IP adress> <port>", argv[0]);
+
+    sockaddr_in clientAdress;
+    inet_aton(argv[1], &clientAdress.sin_addr);
+    clientAdress.sin_port = htons(atoi(argv[2]));
+    clientAdress.sin_family = PF_INET;
+
+    int clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    connect(clientSocket, (sockaddr*)&clientAdress, sizeof(clientAdress));
     
-    connect(sock, (sockaddr*)&adres, sizeof(adres));
-    
-    int x = read(sock, duzybufor, BUFFER_SIZE);
+    int x = read(clientSocket, duzybufor, BUFFER_SIZE);
     perror("Reading from socket");
+    printf("\n%d Data: %s\n",x, duzybufor);
 
-    printf("%d Data: %s\n",x, duzybufor);
+    shutdown(clientSocket, SHUT_RDWR);
+    close(clientSocket);
     return 0;
 }
--- Server/mainServer.cpp
+++ Server/mainServer.cpp
@@ -1,32 +1,63 @@
-#include <iostream>
 #include <sys/socket.h>
-#include <fcntl.h>
 #include "netinet/in.h"
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <errno.h>
- 
+#include <map>
+#include <string>
+#include <error.h>
+
 #define BUFFER_SIZE 512
- 
+
 using namespace std;
 
-int main()
-{
-    char buffer[] = "to gang z albanii otwieraj drzwi";
-    sockaddr_in port;
+// server socket
+int serverFd;
+// Map with user login and socket (FD)
+map<string, int> usersMap;
+//Sample message
+char buffer[] = "to gang z albanii otwieraj drzwi";
 
-    port.sin_addr.s_addr = INADDR_ANY;
-    port.sin_port = htons(3000);
-    port.sin_family = PF_INET;
-    
-    int sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    bind(sock, (sockaddr*)&port, sizeof(port));
+//Functions
+void createServerSocket(int argc, char **argv);
 
-    listen(sock, SOMAXCONN);
+int main(int argc, char **argv) {
 
-    int new_connection = accept(sock, NULL, NULL);
+    createServerSocket(argc, argv);
+
+    int res = listen(serverFd, SOMAXCONN);
+    if(res)
+        error(1, errno, "listen failed");
+
+    int new_connection = accept(serverFd, NULL, NULL);
+    printf("To jest nowe połączenie %d \n", new_connection);
+
+    usersMap.insert(pair<string, int>("login", new_connection));
+
+    printf("To jest w srodku: %d , A tego nie ma: %d \n",usersMap["login"], usersMap["login111"]);
 
     write(new_connection, buffer, sizeof(buffer));
 
+    close(serverFd);
     return 0;
+}
+
+void createServerSocket(int argc, char **argv){
+    if (argc != 3)
+        error(1, 0, "To correctly run type: %s <IP adress> <port>", argv[0]);
+    sockaddr_in serverAddress{
+            .sin_family = AF_INET,
+            .sin_port   = htons(atoi(argv[2])),
+            .sin_addr   = {inet_addr(argv[1])}
+    };
+    serverFd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(serverFd == -1)
+        error(1, errno, "Failed to create server socket");
+
+//    Only allows to faster use the same port - delete later
+    const int one = 1;
+    setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+//    Delete to this place <<<<<<<<<<<
+
+    if(bind(serverFd, (sockaddr*) &serverAddress, sizeof(serverAddress)))
+        error(1,errno,"Failed to bind server address!");
 }
